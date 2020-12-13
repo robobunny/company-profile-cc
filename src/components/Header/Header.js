acd4b0cd981e45952ebc785508bfbf6b0c764153
William C. Duraney <william.duraney@gmail.com>
2020-12-12 21:50:20 -0500

    theme selector

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ThemeSelector from "../ThemeSelector/ThemeSelector";
 import { siteData } from "../../siteData";
-import style from "./header.module.css";
+import styles from "./header.module.css";
 
 const Header = () => {
   const { title, description } = siteData;
 
   return (
-    <header className={style.header}>
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <header className={styles.header}>
+      <div className={styles.headerInfo}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
       <ThemeSelector />
     </header>
   );
--- src/components/ThemeSelector/ThemeSelector.js
+++ src/components/ThemeSelector/ThemeSelector.js
@@ -1,11 +1,49 @@
 import React from "react";
 import useCssDarkMode from "../../hooks/useCssDarkMode";
+import styles from "./themeSelector.module.css";
+
+const Sun = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={styles.darkSVG}
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width={2}
+      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+    />
+  </svg>
+);
+
+const Moon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={styles.lightSVG}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+    />
+  </svg>
+);
 
 const ThemeSelector = () => {
   const { isDarkMode, toggleIsDarkMode } = useCssDarkMode();
   return (
-    <button className="secondary" onClick={toggleIsDarkMode}>
-      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    <button
+      className={isDarkMode ? styles.darkBtn : styles.lightBtn}
+      onClick={toggleIsDarkMode}
+    >
+      {isDarkMode ? <Sun /> : <Moon />}
     </button>
   );
 };
